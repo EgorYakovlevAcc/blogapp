450e6ff6f39848f832dd20824f128c12ebd314df
EgorYakovlev <yakovlev.es@phystech.edu>
2019-05-01 05:28:44 +0300

    example

--- src/main/java/org/blogapp/BlogappApplication.java
+++ src/main/java/org/blogapp/BlogappApplication.java
@@ -1,5 +1,6 @@
 package org.blogapp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 @EnableAutoConfiguration
 public class BlogappApplication extends SpringBootServletInitializer {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogappApplication.class, args);
